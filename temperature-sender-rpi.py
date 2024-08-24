e64dcdc079f4d4b662c220cbd9ec6e8ac1c3fa6f
Filip Jovanovic <civonavoj.pilif@gmail.com>
2024-08-24 18:18:00 +0200

    Use GPIO to display operation status via LED

--- temperature-sender-rpi.py
+++ temperature-sender-rpi.py
@@ -8,7 +8,7 @@ import adafruit_bitbangio as bitbangio
 import digitalio
 import statistics
 
-# Force unbuffered output
+# Force unbuffered output for logging
 sys.stdout.reconfigure(line_buffering=True)
 
 try:
@@ -27,14 +27,26 @@ spi_cs = board.D27
 spi_mosi = board.D10
 spi = bitbangio.SPI(spi_sclk, spi_mosi, spi_miso)
 
+led_pin = board.D8
+led_pin.direction = digitalio.Direction.OUTPUT
+
 cs = digitalio.DigitalInOut(spi_cs)
 
 sensor = adafruit_max31855.MAX31855(spi, cs)
 
-headers: Dict[str, str] = {
-    "Content-Type": "application/json",
-    "x-api-key": API_KEY
-}
+def notifyViaLed(blinkCount: int,  isShort: bool):
+    on_time = 0.1 if isShort else 0.5
+    off_time = 0.1 if isShort else 0.5
+    try:
+        for _ in range(blinkCount):
+            led_pin.value = True
+            time.sleep(on_time)
+            led_pin.value = False
+            time.sleep(off_time)
+    except Exception as e:
+        print(f"LED notification error: {e}")
+    finally:
+        pass
 
 def readTemperature() -> float:
     temperature = sensor.temperature_NIST
@@ -46,17 +58,24 @@ def uploadTemperature(temperature: float, timestamp: int, humanTime: str) -> Non
             "temperature": temperature,
             "timestamp": timestamp
         }
+        headers: Dict[str, str] = {
+            "Content-Type": "application/json",
+            "x-api-key": API_KEY
+        }
         response = requests.post(API_URL, json=payload, headers=headers)
 
         if response.status_code == 200:
+            notifyViaLed(blinkCount = 1, isShort = False)
             print(f"Successfully sent: {temperature}°C at {humanTime}")
         else:
+            notifyViaLed(blinkCount = 4, isShort = True) 
             print(f"Failed to send data: {response.status_code} {response}")
     except Exception as e:
+        notifyViaLed(blinkCount = 5, isShort = True) 
         print(f"Error sending data: {e}")
 
 if __name__ == "__main__":
-    print("Starting temperature monitoring and sending...")
+    print("Starting temperature monitoring and uploading...")
     numberOfReads: int = 0
     temperatures: List[float] = []
     
@@ -76,6 +95,7 @@ if __name__ == "__main__":
                 temperatures = []
             numberOfReads += 1
         except Exception as e:
+            notifyViaLed(blinkCount = 3, isShort = True)
             print(f"Error: {e}")
 
         time.sleep(READ_INTERVAL_SECONDS)
